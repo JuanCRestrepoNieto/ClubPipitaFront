00ad004e031688a9acb70f41039982bfcb36b463
Totta <juanrestrepo300532@correo.itm.edu.co>
2022-10-09 15:20:24 -0500

    Landing page

---
+++ clubpipita/src/LandingPage/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LandingPage(props) {
+    return (
+        <div
+            className="">
+           {props.children}
+        </div>
+    )
+}
+
+export {LandingPage}
