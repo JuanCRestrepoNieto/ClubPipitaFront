243a84726318d401ac354f87852db1281e2061ba
Camilosm20 <madridcamilo606@gmail.com>
2022-10-18 23:09:28 -0500

    idea de formulario en actualizar torneo
    revisen, gracias

--- clubpipita/src/Actualizar/Actualizar.js
+++ clubpipita/src/Actualizar/Actualizar.js
@@ -9,7 +9,7 @@ function Actualizar (props){
         <div className={css.ContenedorBotones}>
             <nav className={css.nav}>
                 <ul className={css.listaBotones}>
-                    <NavLink to='/Nosotros' activeclassname="active" className={`btn btn-success ${css.lista}`} aria-current="page">
+                    <NavLink to='/ActualizarTorneo' activeclassname="active" className={`btn btn-success ${css.lista}`} aria-current="page">
                     <li className={css.textoComponenteLista}>Torneo</li>
                     </NavLink >
                     <NavLink to='/Nosotros' activeclassname="active" className={`btn btn-success ${css.lista}`} aria-current="page">
