2001a2b3bef43a0672d93af95ccaa1c4b6365f89
Cvaz007 <sebastianmoreno301177@correo.itm.edu.co>
2022-10-21 23:06:43 -0500

    mmmm

--- clubpipita/src/Consulta/FRMConsulta/FRMConsultarEquipo.js
+++ clubpipita/src/Consulta/FRMConsulta/FRMConsultarEquipo.js
@@ -3,7 +3,16 @@ import css from './EstilosConsulta.module.css';
 
 function FRMConsultarEquipo(props){
     return(
-        <></> 
+        <div>
+            <form>
+                <label for="nombre">Nombre:  </label><input className="" id="nombre" type ='text' placeholder="Digite el nombre"/>
+            </form>
+            <table>
+                <thead>
+                    <th>nombre</th>
+                </thead>
+            </table>
+        </div>
     );
 }
 
--- clubpipita/src/Consulta/FRMConsulta/FRMConsultarJugador.js
+++ clubpipita/src/Consulta/FRMConsulta/FRMConsultarJugador.js
@@ -3,7 +3,18 @@ import css from './EstilosConsulta.module.css';
 
 function FRMConsultarJugador(props){
     return(
-        <></> 
+        <div>
+            <form>
+                <label for="cedula">Cedula:  </label><input className="" id="cedula" type ='text' placeholder="Digite la cedula"/>
+            </form>
+            <table>
+                <thead>
+                    <th>Cedula</th>
+                    <th>Nombre</th>
+                    <th>Equipo</th>
+                </thead>
+            </table>
+        </div>
     );
 }
 
