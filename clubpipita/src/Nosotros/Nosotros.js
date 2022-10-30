7877c36e54c09b9109c2902ae8c2d4f4b26245cf
leandroku <87959503+leandroku@users.noreply.github.com>
2022-10-30 10:29:52 -0500

    nosotros

--- clubpipita/src/Nosotros/Nosotros.js
+++ clubpipita/src/Nosotros/Nosotros.js
@@ -1,26 +1,56 @@
 import React from "react";
 import css from './Nosotros.module.css'
 
+
 function Nosotros (props){
     return(
         <div>
-            <div
-                className={css.TituloNosotros}>
+            <div className={css.Titulos}>
                     Nosotros
-                </div>
+            </div>
+
+            <br></br>
+            <br></br>
+
+        <div class="row" className={css.centrar}>
+
                 
-                <div
-                className={css.Mision}>
-                    Mision
+                <div class="col-md-2">    
+                        <div className={css.Titulos2}>
+                                Mision
+                        </div>
+
+                        <div className={css.texto2}>
+                        
+                        Ofrecer una mejor experiencia en la obtención de datos deportivos a la ves de dar la garantía de la fidelidad de los datos recopilados
+                                        
+                        </div>
                 </div>
-                <div
-                className={css.Vision}>
-                    Vision
+
+                <div class="col-md-2">    
+                        <div className={css.Titulos2}>
+                                Vision
+                        </div>
+
+                        <div className={css.texto2}>
+                        
+                        Ser la aplicación web #1  en la búsqueda de información fiable y oportuna de los encuentros deportivos
+                                    
+                        </div>
                 </div>
-                <div
-                className={css.QuienesSomos}>
+                <br></br>
+        </div>   
+
+             <div   className={css.Titulos}>
                     Quienes somos
-                </div>
+            </div>
+
+            <div className={css.texto1}>
+            <p>
+            Club pipita esta conformado por  4 estudiantes de desarrollo de sofware del Instituto Tecnológico Metropolitano ITM, con en la intención me facilitar el almacenamiento y consulta de los datos deportivos y lograr así una consolidación más óptima y confiable de los datos medinte un aplicativo web
+            </p>                
+            </div><br></br>
+
         </div>
     );
 }
