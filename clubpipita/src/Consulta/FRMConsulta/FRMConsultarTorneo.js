1bdb28b08562c6ba55e0de3e347b2602bc9fa1c8
Camilosm20 <madridcamilo606@gmail.com>
2022-11-26 18:07:56 -0500

    a

--- clubpipita/src/Consulta/FRMConsulta/FRMConsultarTorneo.js
+++ clubpipita/src/Consulta/FRMConsulta/FRMConsultarTorneo.js
@@ -3,12 +3,12 @@ import css from './EstilosConsulta.module.css';
 
 function FRMConsultarTorneo(props){
     return(
-        <div>
-            <form>
-                <label for="codigo">Codigo:  </label><input className="" id="codigo" type ='text' placeholder="Digite el codigo"/>
+        <div className={css.ConsultarTorneoContainer}>
+            <form className={css.ConsultarTorneo_form_label_text}>
+                <label for="codigo" className={css.label}>Codigo:  </label><input className={css.ConsultarTorneo_text_id} id="codigo" type ='text' placeholder="Digite el codigo"/>
             </form>
-            <table>
-                <thead>
+            <table className={css.ConsultarTorneo_table}>
+                <thead className={css.ConsultarTorneo_head_table}>
                     <th>Codigo</th>
                     <th>Fecha inicio</th>
                     <th>Fecha final</th>
